5f2d3253260d1b5acf93721b340b182c4cd4e1db
adilbek-hub <adilbek.kurmanbekuulu.kk@gmail.com>
2022-10-14 14:52:07 +0600

    weather end

---
+++ weather_app4/lib/constant/api_const.dart
@@ -0,0 +1,14 @@
+class APIConst {
+  static String address(String name) =>
+      'https://api.openweathermap.org/data/2.5/weather?q=$name,&appid=41aa18abb8974c0ea27098038f6feb1b';
+
+  // Сайтты чакыруу максатында getIcon функциясы түзүлдү жана
+  // String iconCode иконканы алуу үчүн, int size иконканын размерин чоңойтуу үчүн
+  static String getIcon(String iconCode, int size) {
+    // чакырылган сайттын ичиндеги wn жана @${size}x иконканын размерин 4х кылуу үчүн жазылды
+    return 'https://openweathermap.org/img/wn/$iconCode@${size}x.png';
+  }
+
+  static String latLongaddres(double lat, double long) =>
+      'https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$long&exclude=hourly,daily,minutely&appid=41aa18abb8974c0ea27098038f6feb1b';
+}
---
+++ weather_app4/lib/constant/app_text.dart
@@ -0,0 +1,3 @@
+class AppText {
+  static const String appBarTitile = 'Тапшырма 9';
+}
